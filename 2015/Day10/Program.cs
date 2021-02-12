0be7f42a3cd13cd1869b50c33307ddbbe07e2b49
DeltaRig <daniprigoli@gmail.com>
2021-02-11 22:12:32 -0300

    teste

---
+++ 2015/Day10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String puzzle = "1211";
+            char temp = puzzle[0];
+            int cont = 0;
+
+            String result = "";
+            
+            for(int i = 0; i < puzzle.Length; i++){
+                if(temp.Equals(puzzle[i])){
+                    cont++;
+                } else {
+                    result += cont + temp;
+                    temp = puzzle[i];
+                    cont = 0;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
