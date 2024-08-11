04d04c9b348682a773ce79902152eba3397bb79d
Rigoli, Daniela <daniela_rigoli@dell.com>
2024-08-11 12:36:22 -0300

    part 1 day 3

---
+++ 2022/Day3/day3.py
@@ -0,0 +1,33 @@
+
+# Daniela Pereira Rigoli
+# August 11th 2024
+# https://adventofcode.com/2022/day/3#part2
+
+# O(n^2)
+# O(max line/2)
+def findMatch(line):
+    list = set()
+    compSize = int((len(line))/2)
+    for a in line[:compSize]:
+        for b in line[compSize:]:
+            if a == b:
+                list.add(a)
+    return list
+            
+# Lowercase item types a through z have priorities 1 through 26.
+# Uppercase item types A through Z have priorities 27 through 52.
+def priority(letter):
+    return ord(letter) - 96 if ord(letter) > 96 else ord(letter) - 38
+
+result = 0
+f = open("input.txt", "r")
+for line in f:
+    match = findMatch(line)
+    for a in match:
+        # print(a,  " | " , priority(a))
+        result += priority(a)
+
+print(result)
+
+
+f.close()
