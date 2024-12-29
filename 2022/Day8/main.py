e197c64ee26ef5317f708e8941eeabe48def863c
DeltaRig <daniprigoli@gmail.com>
2024-12-28 22:40:47 -0300

    day 9 part 2 in progress

--- 2022/Day8/main.py
+++ 2022/Day8/main.py
@@ -1,6 +1,8 @@
 # Daniela Rigoli Dezember 28 2024
 # https://adventofcode.com/2022/day/8
 from pathlib import Path
+
+# Part 1
 # check if is visible
 # considering that it will not receive edges as it will be always visible then don't need to check
 def checkVisible(lines, l, c):
@@ -31,24 +33,75 @@ def checkVisible(lines, l, c):
     return False  # Not visible from any direction
 
 
+def scenicScore(lines, l, c):
+    # Check upwards
+    temp = 0
+    for ll in range(l - 1, -1, -1):
+        if lines[ll][c] >= lines[ll+1][c]:
+            temp+=1
+            break
+        else: temp+=1
+    score = temp
+
+    temp = 0
+    # Check downwards
+    for ll in range(l + 1, len(lines)):
+        if lines[ll][c] >= lines[ll-1][c]:
+            temp+=1
+            break
+        else: temp+=1
+    score = score * temp
+
+    temp = 0
+    # Check left
+    for cc in range(c - 1, -1, -1):
+        if lines[l][cc] >= lines[l][cc+1]:
+            temp+=1
+            break
+        else: temp+=1
+    score = score * temp
+
+    temp = 0
+    # Check right
+    for cc in range(c + 1, len(lines[0])):
+        if lines[l][cc] >= lines[l][cc-1]:
+            temp+=1
+            break
+        else: temp+=1
+
+    score = score * temp
+    return score
 
 def treeTop(file):
     lines = Path(file).read_text().splitlines()
     count = 0
-    print(lines)
+    
     for c in range(1, len(lines[0])-1):
         for l in range(1, len(lines)-1):
-            print(l, " ", c, " ", lines[l][c]," ", checkVisible(lines, l, c))
+            # print(l, " ", c, " ", lines[l][c]," ", checkVisible(lines, l, c))
             if(checkVisible(lines, l, c)):
                 count+=1
-    print(count)
     # sum the edges:
     count += (len(lines[0])-1 + len(lines)-1)*2
-    
+    return count
+
+def treeTopHighScore(file):
+    lines = Path(file).read_text().splitlines()
+# part 2
+#    print("1 2: ", scenicScore(lines, 1,2))
+#    print("3 2 ", scenicScore(lines, 3,2))
+    highest = 0
+    for c in range(len(lines[0])):
+        for l in range(len(lines)):
+            temp = scenicScore(lines, l, c)
+            if(temp > highest):
+                highest = temp
+    return highest
 
     
-    return count
 
 file = 'input.txt'
 result = treeTop(file)
 print("Part 1:", result)
+result = treeTopHighScore(file)
+print("Part 2:", result)
